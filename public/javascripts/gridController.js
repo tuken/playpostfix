abd0204f5a9cd06f57957322044695fab24acacd
Tsukasa Ikawa <t.ikawa@saver.jp>
2016-07-12 21:12:40 +0900

    ui-grid実装中

--- public/javascripts/gridController.js
+++ public/javascripts/gridController.js
@@ -3,15 +3,6 @@ app.controller('gridCtrl', function($scope, $http, $timeout, $location) {
  $scope.domains = [];
  $scope.newDomain = {};
 
- $scope.myData = [
-     {name: "Moroni", age: 50},
-     {name: "Tiancum", age: 43},
-     {name: "Jacob", age: 27},
-     {name: "Nephi", age: 29},
-     {name: "Enos", age: 34}
- ];
- $scope.domainGrid = { data: 'myData' };
-
  function getAllDomain() {
      $http({
          method: 'GET',
